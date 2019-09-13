36a42db311d8aad37be6743b96ab5bad650b93d9
jeonghoheo <heojeongho1992@gmail.com>
2019-09-13 10:34:03 +0900

    Express로 Routes 만들기

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ function handleListening() {
   console.log(`Listening on: http://localhost${PORT}`);
 }
 
+function handleHome(req, res) {
+  res.send("Hello from home");
+}
+
+function handleProfile(req, res) {
+  res.send("You're on my Profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
